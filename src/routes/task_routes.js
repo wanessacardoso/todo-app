7f93a20987106c2df913cb1349ee5d9bb1881425
Samuel Rodrigues <samuelclerod@gmail.com>
2022-04-11 19:29:32 -0300

    feat: done basic tasks api

--- src/routes/task_routes.js
+++ src/routes/task_routes.js
@@ -1,3 +1,4 @@
+const { parse } = require('dotenv');
 const express = require('express');
 
 const tasks_routes = express.Router();
@@ -14,7 +15,69 @@ const generateId = () => {
   return tasks[tasks.length - 1].id + 1;
 }
 
+const isInvalidBody = (body) => {
+  let validFields = ['description', 'done'];
+
+  Object.keys(body).forEach(key => {
+
+    if (!validFields.includes(key)) {
+      return 'Invalid field';
+    }
+
+    if (!body[key]) {
+      return 'Missing fields';
+    };
+  });
+};
+
+tasks_routes.patch('/:id/complete', (req, res) => {
+  const { id } = req.params;
+  const foundTask = tasks.find(task => task.id == id);
+  if (!foundTask) {
+    return res.status(404).send({ error: 'Task not found' });
+  };
+  foundTask.done = true;
+  return res.status(200).send(foundTask);
+})
+
+tasks_routes.put('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const error = isInvalidBody(req.body);
+  if (error) {
+    return res.status(400).send({ error });
+  }
+
+  const foundTask = tasks.find(task => task.id == id);
+  if (foundTask < 0) {
+    return res.status(404).send({ error: 'Task not found' });
+  }
+
+  Object.assign(foundTask, req.body);
+
+  return res.send(foundTask);
+});
+
+tasks_routes.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  const taskIndex = tasks.findIndex(task => task.id === parseInt(id));
+
+  if (taskIndex < 0) {
+    return res.status(404).send({ error: 'Task not found' });
+  }
+
+  tasks.splice(taskIndex, 1);
+
+  return res.status(204).send();
+});
+
 tasks_routes.post('/', (req, res) => {
+
+  const error = isInvalidBody(req.body);
+  if (error) {
+    return res.status(400).send({ error });
+  }
+
   const { description } = req.body;
   if (!description) {
     return res.status(400).send({ error: 'You must provide a description' });
